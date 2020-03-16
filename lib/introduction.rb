b94785d4d49681d3d27e552b83a99c906f3c47d3
Xa Sherrod <xaviersherrod99@yahoo.com>
2020-03-16 07:36:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,8 +8,9 @@ def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
-
+def introduction_with_language_optional(name)
+  puts "Hi, my name is #{name} and I am learning to program in Ruby."
+end
 
 
 
